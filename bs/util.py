9fcd75e823db8f6f795e21031060a5b6b3291208
Kuba Marek <blue.cube@seznam.cz>
2015-11-11 15:18:40 +0100

    Utility function for hashing iterables.

--- bs/util.py
+++ bs/util.py
@@ -13,13 +13,24 @@ def mmap_file(path):
             with mmap.mmap(fp.fileno(), 0, access=mmap.ACCESS_READ) as mm:
                 yield mm
 
+def sha1_iterable(*iterables):
+    """ Return a hash of all elements in iterable, each followed by a null byte.
+    Iterable must contain bytes (used as is), or strings (encoded to utf-8)"""
+    hasher = hashlib.sha1()
+    for iterable in iterables:
+        for item in iterable:
+            try:
+                hasher.update(item)
+            except TypeError:
+                hasher.update(str(item).encode("utf-8"))
+            hasher.update(b"\0")
+    return hasher.digest()
+
 def sha1_file(path):
     """ Read a file (pathlib.Path) and return hash of its content. """
-
     hasher = hashlib.sha1()
     with mmap_file(path) as mm:
         hasher.update(mm)
-
     return hasher.digest()
 
 # This version is simpler, but consistently slower by about 25%
@@ -34,4 +45,3 @@ def sha1_file(path):
 #                hasher.update(block)
 #
 #    return hasher.digest()
-
--- tests/util_test.py
+++ tests/util_test.py
@@ -5,13 +5,32 @@ import pathlib
 
 import bs.util
 
-def check_file_sha1(directory, filename, content, sha1):
-    path = os.path.join(directory, filename)
-    with open(path, "w") as fp:
-        fp.write(content)
-    eq_(bs.util.sha1_file(pathlib.Path(path)), bytearray.fromhex(sha1))
-
 def sha1_file_test():
     with tempfile.TemporaryDirectory() as d:
-        check_file_sha1(d, "empty.txt", "", "da39a3ee5e6b4b0d3255bfef95601890afd80709")
-        check_file_sha1(d, "abc.txt", "abc", "a9993e364706816aba3e25717850c26c9cd0d89d")
+        def check(filename, content, sha1):
+            path = os.path.join(d, filename)
+            with open(path, "w") as fp:
+                fp.write(content)
+            eq_(bs.util.sha1_file(pathlib.Path(path)), bytearray.fromhex(sha1))
+
+        check("empty.txt", "", "da39a3ee5e6b4b0d3255bfef95601890afd80709")
+        check("abc.txt", "abc", "a9993e364706816aba3e25717850c26c9cd0d89d")
+
+def sha1_iterable_test():
+    hashes = set()
+    def check_unique(*args):
+        nonlocal hashes
+        h = bs.util.sha1_iterable(*args)
+        assert h not in hashes
+
+    list1 = [1, "a", b"b"]
+    list2 = list(range(5))
+
+    check_unique([])
+    check_unique(list1)
+    check_unique(reversed(list1))
+    check_unique(list1 + ["a"])
+    check_unique(iter(list2))
+    check_unique(list1, list2)
+
+    eq_(bs.util.sha1_iterable(list1, list2), bs.util.sha1_iterable(list1 + list2))
